f68d001579638da10a9fb3bd6468da317026206f
bhoid <bhoid.b@gmail.com>
2021-03-30 20:56:39 +1100

    Update pixabay_attribution_helper_new.js

--- helpers/pixabay_attribution_helper_new.js
+++ helpers/pixabay_attribution_helper_new.js
@@ -100,7 +100,7 @@ $(function() {
 
 
             $('small').each(function(index, el) {
-                $(this).html(statement + ', accessed ' + today);
+                $(this).html(statement + ', <a href="https://creativecommons.org/licenses/by/3.0/au/" target="_blank">CC BY 3.0 AU</a>.');
             });
 
             $('#embedder button').click(function(event) {
