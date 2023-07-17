6d92d0f25401f4802a12984e50b373ef3ba27fd7
bhoid <bhoid.b@gmail.com>
2023-07-17 15:55:00 +1000

    Update attribution_maker2.js

--- helpers/attribution_maker2.js
+++ helpers/attribution_maker2.js
@@ -46,7 +46,7 @@ $(function() {
             org: null,
             cols: "col-5",
             percent: "42",
-            collapsed: true,
+            collapsed: false,
             layout: "bootstrap",
             classes: ["border", "bg-light"]
         },
